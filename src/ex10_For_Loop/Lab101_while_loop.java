b51a857595c7d64eb98d8b3f9dfa6cee22f0d1f3
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-03-21 12:15:09 +0530

    21th march

--- src/ex10_For_Loop/Lab101_while_loop.java
+++ src/ex10_For_Loop/Lab101_while_loop.java
@@ -1,28 +1,17 @@
 package ex10_For_Loop;
 
 public class Lab101_while_loop {
-    int i = 0; // Init
-    while(i< 10)
+    public static void main(String[] args) {
+        int i = 0;  // Init
+        while ( i < 10) { // condition must be true to enter the loop.
+            //   System.out.println("start the program");
+            System.out.println(i);
+            i++;  // updation
+        }
+        // I , C , U
+        for ( int j = 0; j < 10; j++) {
+            System.out.println(j);
 
-    {
-
-        // condition ( must be true to enter the loop )
-        System.out.println("start the problem");
-        System.out.println(i);
-        i++;  // updation
-
-    }
-    // I , C , U
-    for(
-    int j = 0;
-    j< 10;j++)
-
-    {
-
-
-    }
-
-}
-
-
+        }
+        }
 }
