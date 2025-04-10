750614a22de99d05465d02f2839ec2e3e4498387
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-10 09:53:42 +0530

    10th April  2025

--- src/ex31_Exception/Lab205_exception_explained01.java
+++ src/ex31_Exception/Lab205_exception_explained01.java
@@ -2,6 +2,16 @@ package ex31_Exception;
 
 public class Lab205_exception_explained01 {
     public static void main(String[] args) {
+        System.out.println("Starting the program");
+        String input_user = args[0]; // java.lang.ArrayIndexOutOfBoundsException
+        int a = Integer.parseInt(input_user);
+        int output = 100/a; // java.lang.ArithmeticException: / by zero
+        System.out.println(output);
+        System.out.println("end of the program!!!");
+
+
+
+        // divide by zero-> ?
 
     }
 }
